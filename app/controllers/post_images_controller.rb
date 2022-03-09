2dfb7f2c33d0bbaa8da16195e185cac308a0b9bb
Appi66 <toy.alo.ldh@gmail.com>
2022-03-09 01:54:45 +0000

    [Add]投稿編集機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,30 +2,41 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
-  end  
+  end
 
   def index
     @post_images = PostImage.all
   end
 
-  def show
+  def edit
+    @post_image = PostImage.find(params[:id])
   end
-  
+
+  def update
+    post_image = PostImage.find(params[:id])
+    post_image.update(post_image_params)
+    redirect_to post_image_path(post_image.id)
+  end
+
+  def show
+    @post_image =PostImage.find(params[:id])
+  end
+
   def destroy
-  end 
-  
-  
+  end
+
+
   # 投稿データのストロングパラメーター
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:image, :caption)
-  end  
-  
+  end
+
 end
