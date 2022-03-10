c218dda5708aa8f12a36ad423f0cb3c7b3d43d65
Appi66 <toy.alo.ldh@gmail.com>
2022-03-10 05:58:24 +0000

    [Create]PostCommentモデル

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,7 +11,7 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.all.order(created_at: :desc)
   end
 
   def edit
