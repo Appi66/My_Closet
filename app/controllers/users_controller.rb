f0580faf6c0677fbe06290c289c5bcea884b6f63
Appi66 <toy.alo.ldh@gmail.com>
2022-04-17 05:58:01 +0000

    [Add]マイページにお気に入りタブを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.order(created_at: :desc).page(params[:page])
+    favorites = Favorite.where(user_id: @user.id).pluck(:post_image_id)
+    @favorite_post_images = PostImage.find(favorites)
+    @favorite_post_images = Kaminari.paginate_array(@favorite_post_images).page(params[:page])
   end
 
   def edit
